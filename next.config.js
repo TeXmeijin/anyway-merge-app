3a1cca6073cfa088b4608bf6972319802066d159
Aiji Uejima <aiji42@gmail.com>
2021-12-04 14:23:25 +0900

    fix: realtime cursor

--- next.config.js
+++ next.config.js
@@ -3,8 +3,5 @@ module.exports = {
   reactStrictMode: true,
   env: {
     ABLY_IS_ACTIVE: !!process.env.ABLY_API_KEY,
-    ABLY_API_ENDPOINT: process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}/api/ably-create-token`
-      : `http://localhost:3000/api/ably-create-token`,
   },
 };
