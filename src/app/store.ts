fb1fb22b533f4c7625b5a7a0ccc3df0b74e5e878
SatakeYusuke19920527 <satkyusk@icloud.com>
2021-12-05 10:20:38 +0900

    reduxtoolkitをインストールしました。@fe_js_engineer

---
+++ src/app/store.ts
@@ -0,0 +1,17 @@
+import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import userReducer from '../features/userSlice';
+
+export const store = configureStore({
+  reducer: {
+    user: userReducer,
+  },
+});
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
--- src/components/atoms/navbar/index.tsx
+++ src/components/atoms/navbar/index.tsx
@@ -1,11 +1,27 @@
-import { useRouter } from "next/router";
-import Link from "next/link";
-import { signIn, signOut, useSession } from "next-auth/react";
-import styles from "./styles.module.css";
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+import { signIn, signOut, useSession } from 'next-auth/react';
+import styles from './styles.module.css';
+import { login, logout } from '../../../features/userSlice';
+import { useAppDispatch } from '../../../hooks/useRTK';
 
 const Navbar: React.FC = () => {
   const { data: session, status } = useSession();
   const router = useRouter();
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatchReduxStore();
+  }, [status]);
+
+  const dispatchReduxStore = () => {
+    if (status === 'authenticated') {
+      dispatch(login({ displayName: session?.user.name }));
+    } else {
+      dispatch(logout());
+    }
+  };
 
   return (
     <div className={styles.navbar}>
@@ -14,16 +30,16 @@ const Navbar: React.FC = () => {
           <a>サイトロゴ</a>
         </Link>
       </div>
-      {status === "authenticated" && (
+      {status === 'authenticated' && (
         <div>
-          ログイン中：{session?.user.name}さん{" "}
-          <button onClick={() => router.push("/dashboard")}>
+          ログイン中：{session?.user.name}さん{' '}
+          <button onClick={() => router.push('/dashboard')}>
             ユーザー専用ページ
-          </button>{" "}
+          </button>{' '}
           <button onClick={() => signOut()}>ログアウト</button>
         </div>
       )}
-      {status === "unauthenticated" && (
+      {status === 'unauthenticated' && (
         <div>
           未ログイン <button onClick={() => signIn()}>ログイン</button>
         </div>
