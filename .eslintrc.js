47c299634913d00212d2b4ea839bff29604009ff
yokinist <ikd618@gmail.com>
2021-12-06 19:08:13 +0900

    chore: setup unused-imports

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,9 @@
 /** @type {import('eslint-config-next')*/
 module.exports = {
   extends: ["next", "next/core-web-vitals", "prettier"],
+  plugins: ["unused-imports"],
   rules: {
     "@next/next/no-img-element": "off",
+    "unused-imports/no-unused-imports": "error",
   },
 };
