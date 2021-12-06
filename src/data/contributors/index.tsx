d0a86adab50021e9514d8f85d9bf595bc9c5184a
yokinist <ikd618@gmail.com>
2021-12-06 20:23:22 +0900

    refactor: contributor page

---
+++ src/data/contributors/index.tsx
@@ -0,0 +1,99 @@
+import { Meijin } from "./components";
+import { Contributor } from "~/types";
+
+export const contributors: Contributor[] = [
+  {
+    name: "meijin",
+    slug: "meijin",
+    description: "株式会社NoSchool CTO\n本企画の主催です",
+    links: [
+      {
+        name: "Twitter",
+        url: "https://twitter.com/meijin_garden",
+      },
+      {
+        name: "meijin.dev",
+        url: "https://meijin.dev",
+      },
+    ],
+    additionalSection: <Meijin />,
+  },
+  {
+    name: "sassy",
+    slug: "sassy",
+    description:
+      "株式会社カケハシ エンジニア\nフロントエンドエンジニア兼バックエンドエンジニア",
+    links: [
+      {
+        name: "Twitter",
+        url: "https://twitter.com/sassy_watson",
+      },
+    ],
+  },
+  {
+    name: "Motoki Watanabe @XxGodmoonxX",
+    slug: "motoki",
+    description:
+      "デザイン会社のソフトウェアエンジニア（フロントエンド）をしつつたまにクラブでVJ（映像演出）をしています",
+    links: [
+      {
+        name: "Twitter",
+        url: "https://twitter.com/XxGodmoonxX",
+      },
+      {
+        name: "motoki-watanabe.net",
+        url: "https://www.notion.so/motokiwatanabe/Motoki-Watanabe-70212745c77a483fa3a96b7c6217d801",
+      },
+      {
+        name: "技術ブログ",
+        url: "https://tech.motoki-watanabe.net/",
+      },
+    ],
+  },
+  {
+    name: "Daiki OKUMURA",
+    slug: "okmr-d",
+    description: "ウェッブエンジニャー🐱",
+    links: [
+      {
+        name: "Twitter",
+        url: "https://twitter.com/okumura_daiki",
+      },
+    ],
+  },
+  {
+    name: "Aiji Uejima",
+    slug: "aiji",
+    description:
+      "株式会社エイチームライフスタイルでリードエンジニアやってます\nトップページのリアルタイムカーソルを実装しました\n運が良ければ誰かのマウスカーソルが見れます\n自分以外に誰もいなければ、別ウィンドウで同時に2画面開いてみてください",
+    links: [
+      {
+        name: "Twitter",
+        url: "https://twitter.com/aiji42_dev",
+      },
+      {
+        name: "GitHub",
+        url: "https://github.com/aiji42",
+      },
+    ],
+  },
+  {
+    name: "yokinist",
+    slug: "yokinist",
+    description: "よろしくお願いします〜〜〜！",
+    links: [
+      {
+        name: "Twitter",
+        url: "https://twitter.com/yokinist",
+      },
+      {
+        name: "GitHub",
+        url: "https://github.com/yokinist",
+      },
+      {
+        name: "yokinist.me",
+        url: "https://yokinist.me/",
+      },
+    ],
+  },
+];
