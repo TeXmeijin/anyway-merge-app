11761947a94b9049452b97c4ff091e5af4081a95
tomon9086 <tomon9086@gmail.com>
2021-12-08 12:28:42 +0900

    show GitHub avatar in profile page with octokit

--- src/pages/contributors/[slug]/index.tsx
+++ src/pages/contributors/[slug]/index.tsx
@@ -1,8 +1,10 @@
 import ErrorPage from "next/error";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import styles from "./styles.module.css";
 import ContributorHeading from "~/components/atoms/contributer-heading/index";
 import { contributors } from "~/data";
+import { getGitHubUser } from "~/data/github";
 import { convertTextToHtml } from "~/libs/convertTextToHtml";
 import { encodeHtml } from "~/libs/encodeHtml";
 
@@ -10,8 +12,19 @@ function ContributorPage() {
   const {
     query: { slug },
   } = useRouter();
+  const [avatarUrl, setAvatarUrl] = useState<string>();
 
   const contributor = contributors.find((c) => c.slug === slug);
+
+  useEffect(() => {
+    const githubUrl = contributor?.links?.find(
+      (link) => link.name === "GitHub"
+    )?.url;
+    githubUrl &&
+      getGitHubUser({ githubUrl }).then((user) => {
+        setAvatarUrl(user.avatar_url as string);
+      });
+  }, [contributor?.links]);
 
   if (!contributor) return <ErrorPage statusCode={404} />;
 
@@ -19,7 +32,9 @@ function ContributorPage() {
     <div className={styles.container}>
       <div className={styles.content}>
         <div className={styles.content__inner}>
-          <ContributorHeading>{contributor.name}</ContributorHeading>
+          <ContributorHeading iconUrl={avatarUrl}>
+            {contributor.name}
+          </ContributorHeading>
           <p
             dangerouslySetInnerHTML={encodeHtml(
               convertTextToHtml(contributor?.description)
