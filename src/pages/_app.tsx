43191b8fccd68c738b9e868929371e00106f1ddd
kanoi-y <furafuratonbo@gmail.com>
2021-12-08 15:19:21 +0900

    ✨ 雪だるまの画像を追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,6 @@
 import { SessionProvider } from "next-auth/react";
+import Image from "next/image";
+import { useState } from "react";
 import { Provider } from "react-redux";
 import Snowfall from "react-snowfall";
 import { VFXProvider } from "react-vfx";
@@ -7,12 +9,44 @@ import { store } from "~/app/store";
 import "~/styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [isFalling, setIsFalling] = useState(true);
+
+  const handleChangeFallingButton = () => {
+    setIsFalling((prevValue) => !prevValue);
+  };
+
   return (
     <SessionProvider session={pageProps.session}>
       <VFXProvider>
         <Provider store={store}>
           <div style={{ position: "relative", width: "100%", height: "100%" }}>
-            <Snowfall />
+            {isFalling && <Snowfall />}
+            <div
+              style={{
+                width: "70px",
+                position: "absolute",
+                bottom: "10px",
+                right: "10px",
+                cursor: "pointer",
+              }}
+              onClick={handleChangeFallingButton}
+            >
+              {isFalling ? (
+                <Image
+                  src="/images/snowman_yukidaruma_man.png"
+                  alt="雪だるま"
+                  width="695"
+                  height="800"
+                />
+              ) : (
+                <Image
+                  src="/images/snowman_yukidaruma_tokeru.png"
+                  alt="雪だるま"
+                  width="683"
+                  height="683"
+                />
+              )}
+            </div>
             <Component {...pageProps} />
           </div>
         </Provider>
