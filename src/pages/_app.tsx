3b132e1029f3b71ee3ddc7f29ff778b5096f2254
kanoi-y <furafuratonbo@gmail.com>
2021-12-08 14:05:07 +0900

    ➕ react-snowfalをインストール

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,17 @@
-import "~/styles/globals.css";
 import { SessionProvider } from "next-auth/react";
 import { Provider } from "react-redux";
+import Snowfall from "react-snowfall";
 import { VFXProvider } from "react-vfx";
 import type { AppProps } from "next/app";
 import { store } from "~/app/store";
+import "~/styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
       <VFXProvider>
         <Provider store={store}>
+          <Snowfall />
           <Component {...pageProps} />
         </Provider>
       </VFXProvider>
