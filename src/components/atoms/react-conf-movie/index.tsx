a1f5b794234247729b542e6bda68b3bf071a5c6d
tossy_yukky <snowsphere.burned@gmail.com>
2021-12-10 18:16:24 +0900

    modify: calculate width & height

--- src/components/atoms/react-conf-movie/index.tsx
+++ src/components/atoms/react-conf-movie/index.tsx
@@ -1,27 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "~/components/atoms/react-conf-movie/styles.module.css";
 import { useWindowDimensions } from "~/hooks/useWindowDimensions";
 
 const ReactConfMovie: React.FC = () => {
-  const { width } = useWindowDimensions();
-  // height / width
-  const aspectRatio = 0.5625;
-
+  const title = "YouTube video player";
   const url = "https://www.youtube.com/embed/8dUpL8SCO1w?autoplay=1&mute=1";
-  const movieWidth = width == 0 ? 560 : width * 0.38;
-  const movieHeight = movieWidth * aspectRatio;
-
   const frameBorder = "0";
   const allow =
     "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture;";
+  const { width } = useWindowDimensions();
+  const [movieWidth, setMovieWidth] = useState<number>();
+  const [movieHeight, setMovieHeight] = useState<number>();
+  // 16:9
+  const aspectRatio = 0.5625;
+
+  useEffect(() => {
+    let defaultWidth = 560;
+    if (width < 768) {
+      defaultWidth = 400;
+    }
+    setMovieWidth(defaultWidth);
+    setMovieHeight(defaultWidth * aspectRatio);
+  }, []);
 
   return (
     <div className={styles.react_conf_movie}>
       <iframe
         width={`${movieWidth}`}
-        height={movieHeight}
+        height={`${movieHeight}`}
         src={url}
-        title="YouTube video player"
+        title={title}
         frameBorder={frameBorder}
         allow={allow}
         allowFullScreen
