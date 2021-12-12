05c4350d42d0c692b89721fd53387c7a2115ca4d
kobori.hikaru <kobori.hikaru@a-hikkoshi.co.jp>
2021-12-12 16:46:55 +0900

    urlインポートの設定追加

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const path = require("path");
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  experimental: {
+    urlImports: ['https://assets1.lottiefiles.com/packages/']
+  },
   env: {
     ABLY_IS_ACTIVE: !!process.env.ABLY_API_KEY,
   },
