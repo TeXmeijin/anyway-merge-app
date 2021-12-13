7247dae9b3bcaff3a41320e449bd4487be5cfd72
ナモすけ <dynamorollerbot@gmail.com>
2021-12-14 05:05:24 +0900

    コンポーネント分割

--- src/components/atoms/contributors-list/index.tsx
+++ src/components/atoms/contributors-list/index.tsx
@@ -2,20 +2,32 @@ import Link from "next/link";
 import { contributors } from "~/data";
 import { pagesPath } from "~/libs/$path";
 import styles from "~/styles/Home.module.css";
+import { Contributor } from "~/types";
 
 const ContributorsList = () => {
   return (
     <div>
       <ul className={styles.list}>
         {contributors.map((member) => (
-          <li key={member.slug} className={styles.listitem}>
-            <Link href={pagesPath.contributors._slug(member.slug).$url()}>
-              <a>{member.name}</a>
-            </Link>
-          </li>
+          <ContributorsListMember key={member.toString()} member={member} />
         ))}
       </ul>
     </div>
+  );
+};
+
+const ContributorsListMember = ({
+  member,
+}: {
+  key: string;
+  member: Contributor;
+}) => {
+  return (
+    <li key={member.slug} className={styles.listitem}>
+      <Link href={pagesPath.contributors._slug(member.slug).$url()}>
+        <a>{member.name}</a>
+      </Link>
+    </li>
   );
 };
 
