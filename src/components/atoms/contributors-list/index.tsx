5b096681751e5a1ca17650f8869b2f05b0ae00af
ナモすけ <dynamorollerbot@gmail.com>
2021-12-14 05:14:14 +0900

    contributorsにアイコン表示

--- src/components/atoms/contributors-list/index.tsx
+++ src/components/atoms/contributors-list/index.tsx
@@ -1,5 +1,8 @@
 import Link from "next/link";
+import { useEffect, useState } from "react";
+import ContributorHeading from "~/components/atoms/contributer-heading/index";
 import { contributors } from "~/data";
+import { getGitHubUser } from "~/data/github";
 import { pagesPath } from "~/libs/$path";
 import styles from "~/styles/Home.module.css";
 import { Contributor } from "~/types";
@@ -22,10 +25,24 @@ const ContributorsListMember = ({
   key: string;
   member: Contributor;
 }) => {
+  const [avatarUrl, setAvatarUrl] = useState<string>();
+  useEffect(() => {
+    const githubUrl = member?.links?.find(
+      (link) => link.name === "GitHub"
+    )?.url;
+    githubUrl &&
+      getGitHubUser({ githubUrl }).then((user) => {
+        setAvatarUrl(user.avatar_url as string);
+      });
+  }, [member?.links]);
   return (
     <li key={member.slug} className={styles.listitem}>
       <Link href={pagesPath.contributors._slug(member.slug).$url()}>
-        <a>{member.name}</a>
+        <a>
+          <ContributorHeading iconUrl={avatarUrl}>
+            {member.name}
+          </ContributorHeading>
+        </a>
       </Link>
     </li>
   );
