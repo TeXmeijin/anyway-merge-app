ccb7480b0b03bfcb561f59ab2a467b689a632a73
Shoichi Namba <alphabrend@gmail.com>
2021-12-15 09:53:35 +0900

    アバター素材を追加

--- src/hooks/useContributors.ts
+++ src/hooks/useContributors.ts
@@ -1,5 +1,7 @@
 import { useCallback } from "react";
 import { atom, useRecoilState } from "recoil";
+import { contributors } from "~/data";
+import { avatars } from "~/data/avatars";
 import { getGitHubUser } from "~/data/github";
 import { Contributor } from "~/types";
 
@@ -11,16 +13,31 @@ const avatarUrlsState = atom<{ [key: string]: string }>({
 export const useContributors = () => {
   const [avatarUrls, setAvatarUrls] = useRecoilState(avatarUrlsState);
 
+  const getGitHubIconUrl = useCallback((contributor: Contributor) => {
+    return contributor.links?.find((link) => link.name === "GitHub")?.url;
+  }, []);
+
+  const getDefaultAvatarUrl = useCallback((contributor: Contributor) => {
+    const index = contributors.findIndex((c) => c.slug === contributor.slug);
+    const noIconCount = contributors
+      .slice(0, index)
+      .filter((c) => !getGitHubIconUrl(c)).length;
+    const avatarIndex = noIconCount % avatars.length;
+    return `/images/avatars/${avatars[avatarIndex]}`;
+  }, []);
+
   const loadAvatarUrl = useCallback(
     async (contributor: Contributor) => {
       if (avatarUrls[contributor.slug]) {
         return;
       }
 
-      const githubUrl = contributor.links?.find(
-        (link) => link.name === "GitHub"
-      )?.url;
+      const githubUrl = getGitHubIconUrl(contributor);
       if (!githubUrl) {
+        setAvatarUrls((avatarUrls) => ({
+          ...avatarUrls,
+          [contributor.slug]: getDefaultAvatarUrl(contributor),
+        }));
         return;
       }
 
